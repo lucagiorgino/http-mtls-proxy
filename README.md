04a4e46fc54815977bb33286a7f181baebc7d73e
Luca Giorgino <19236900+lucagiorgino@users.noreply.github.com>
2024-12-24 12:12:30 +0100

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # http-mtls-proxy
+
+## Feature
+
+Feature of [mitmproxy](https://docs.mitmproxy.org/stable/):
+
+- Intercept HTTP & HTTPS requests and responses and modify them on the fly
+- Save complete HTTP conversations for later replay and analysis
+- Replay the client-side of an HTTP conversation
+- Replay HTTP responses of a previously recorded server
+- Reverse proxy mode to forward traffic to a specified server
+- Transparent proxy mode on macOS and Linux
+- Make scripted changes to HTTP traffic using Python
+- SSL/TLS certificates for interception are generated on the fly
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,13 @@ use std::net::SocketAddr;
 
 use bytes::Bytes;
 use http_body_util::Full;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::{TokioIo, TokioTimer};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 // An async function that consumes a request, does nothing with it and returns a
 // response.
-async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
-}
+// async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
+//     Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
+// }
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -23,7 +19,9 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
 
     // Bind to the port and listen for incoming TCP connections
+    // Bind the listener to the address
     let listener = TcpListener::bind(addr).await?;
+
     println!("Listening on http://{}", addr);
     loop {
         // When an incoming TCP connection is received grab a TCP stream for
@@ -34,24 +32,33 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         // has work to do. In this case, a connection arrives on the port we are listening on and
         // the task is woken up, at which point the task is then put back on a thread, and is
         // driven forward by the runtime, eventually yielding a TCP stream.
-        let (tcp, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
         // Use an adapter to access something implementing `tokio::io` traits as if they implement
         // `hyper::rt` IO traits.
-        let io = TokioIo::new(tcp);
+        // let io = TokioIo::new(tcp);
+
+        println!("{:?}", socket);
+        
+        tokio::spawn(handle_client(socket));
 
         // Spin up a new task in Tokio so we can continue to listen for new TCP connection on the
         // current task without waiting for the processing of the HTTP1 connection we just received
         // to finish
-        tokio::task::spawn(async move {
-            // Handle the connection from the client using HTTP1 and pass any
-            // HTTP requests received on that connection to the `hello` function
-            if let Err(err) = http1::Builder::new()
-                .timer(TokioTimer::new())
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                println!("Error serving connection: {:?}", err);
-            }
-        });
+        // tokio::task::spawn(async move {
+        //     // Handle the connection from the client using HTTP1 and pass any
+        //     // HTTP requests received on that connection to the `hello` function
+        //     if let Err(err) = http1::Builder::new()
+        //         .timer(TokioTimer::new())
+        //         .serve_connection(io, service_fn(hello))
+        //         .await
+        //     {
+        //         println!("Error serving connection: {:?}", err);
+        //     }
+        // });
     }
+}
+
+// Handle client connection
+async fn handle_client(mut socket: TcpStream) {
+    todo!();
 }
